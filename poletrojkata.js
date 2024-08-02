d566d6c03890ca6f1c904533b128e6d4e577c0ef
onewayyyy4 <onewayyyy@protonmail.com>
2024-08-02 23:44:37 +0300

    Add API integration

---
+++ poletrojkata.js
@@ -0,0 +1,6 @@
+var a = prompt('Podaj a');
+var h = prompt('Podaj h');
+
+var triangleArea = (a * h/2);
+alert = ('pole trójkąta' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea)
